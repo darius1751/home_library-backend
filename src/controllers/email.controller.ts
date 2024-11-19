ad8a3fd36d2ec60dc652c6127d17dbedd14abfa4
Dani <dustyglass@gmail.com>
2024-11-19 18:57:55 -0300

    add last name

--- src/controllers/email.controller.ts
+++ src/controllers/email.controller.ts
@@ -2,12 +2,12 @@ import { transport } from '../config/email.config';
 import { Request, Response } from "express";
 
 export const sendBookEmail = async (request: Request, response: Response) => {
-   const {sender, receiver, name, friend, id} = request.body;
+   const {sender, receiver, name, friend, id, lastname} = request.body;
     await transport.sendMail({
-        from: sender,
+        from: `${name} ${lastname} <${sender}>`,
         to: receiver,
         cc: sender,
-        subject: `${name} sent you their book list`,
+        subject: `${name} ${lastname} sent you their book list`,
         text: `Hi ${friend}! \n\n Here are all the books in my library. You can see what books I have and what books are in my wishlist. \n\n Click the link below to see the list: \n\n http://localhost:3000/dashboard/books/${id} \n\n Love, ${name}`,
     })
 }
