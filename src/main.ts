e038ec3ceb5869daa24c0b98f66aff34ae91801d
Dani <dustyglass@gmail.com>
2024-12-03 08:52:27 -0300

    change variable name

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ import { avatarRouter } from './routers/avatar.router';
 const main = async () => {
     const app = express();
     config();
-    app.use(cors({ credentials: true, origin: [`${process.env.HOME}`] }));
+    app.use(cors({ credentials: true, origin: [`${process.env.HOME_URL}`] }));
     // Remove in production version
     app.use(morgan('tiny'));
     app.use(helmet());
